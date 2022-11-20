ddb47ea9c4f7d8c6ee375eefa2a8c3c38f00987d
Daniil Mosyagin <danok@list.ru>
2022-11-20 21:38:16 +0300

    ready for create instance

--- cloud-terraform/otus_1.tf
+++ cloud-terraform/otus_1.tf
@@ -9,4 +9,27 @@ terraform {
 
 provider "yandex" {
   zone = "ru-central1-a"
+  service_account_key_file = "/home/daniil/OTUS/key.json"
+  folder_id = "b1go7pq0c5r5n54tpjj3"
+}
+resource "yandex_compute_instance" "terraform-first" {
+  name = "terraform-first"
+  
+  resources {
+    cores = 2
+    memory = 2
+}
+  boot_disk {
+    initialize_params {
+      image_id = "fd89jk9j9vifp28uprop"   
+    }
+
+  }
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_ed25519.pub")}"
+  }
+  network_interface {
+    subnet_id = "e9bbn4t2n1m995m76cbi"
+    nat = true
+  }
 }
